fe7256947ab44bd6c7698dc2fd02bf02b2cf9845
Ansruta <75419631+Ansruta@users.noreply.github.com>
2022-05-20 10:33:53 +0530

    Update swappingFiles.py

--- swappingFiles.py
+++ swappingFiles.py
@@ -1,17 +1,15 @@
-def swappingFile():
-    file1=input("Enter file name:")
-    data_a=open(file1,'r')
-
-    file2=input("Enter second file name:")
-    data_b=open(file2,'r')
-
-    a=open(file1,'w')
-    a.write(data_b)
-
-    b=open(file2,'w')
-    b.write(data_a)
-
-    print("file1="+ a)
-    print("file2"+ b)
-
-swappingFile()
+def swapFile():
+    file1 = input("enter files name: ") 
+    file2 = input("enter files name: ")
+    
+    with open(file1, 'r') as a: 
+        data_a = a.read() 
+    with open(file2, 'r') as b: 
+        data_b = b.read()
+        
+    with open(file1, 'w') as a: 
+        a.write(data_b)
+    with open(file2, 'w') as b:
+        b.write(data_a) 
+        
+swapFile()
